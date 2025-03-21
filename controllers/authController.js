356e56e0b26e6644af60d9001fdc2626d4907c43
Oduala Oluwatosin <oluwatosinoduala@gmail.com>
2025-03-21 13:27:16 +0100

    fixing the server error file for the signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,6 @@
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
-const bcrypt = require('bcrypt');  // Only need one bcrypt library
-const bcryptjs = require('bcryptjs')
+const bcrypt = require('bcrypt');  // Choose one bcrypt library
 
 const signup = async (req, res) => {
     try {
@@ -12,7 +11,6 @@ const signup = async (req, res) => {
           $or: [{ username }, {email} ],
         });
     
-
         // so now we check, if the user exists, we return a response dynamic enough to tell us if it's user name or password
         if (existingUser) {
           if (existingUser.username === username) {
@@ -27,19 +25,19 @@ const signup = async (req, res) => {
           }
         }
     
+        // Hash the password before saving
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
         
-    
         // Create new user with hashed password
         const newUser = new User({ 
             firstName, 
             lastName, 
             username, 
             email, 
-            password 
+            password: hashedPassword // Save the hashed password
         });
         await newUser.save();
-
-        
     
         res.status(201).json({
           success: true,
@@ -50,80 +48,58 @@ const signup = async (req, res) => {
             lastName: newUser.lastName,
             username: newUser.username,
             email: newUser.email,
-            password:newUser.password
+            // Don't return the password
           },
         });
       } catch (error) {
-        console.log("Signup Error:", error);
+        console.log("Signup Error:", error.message); // Log more details
         res.status(500).json({ message: "Error signing up users" });
       }
-    };
+};
 
+const signin = async (req, res) => {
+  try {
+    // Get the data from the request body (coming from the frontend)
+    const { email, password } = req.body;
 
-    const signin = async (req, res) => {
-      try {
-        // Get the data from the request body (coming from the frontend)
-        const { email, password } = req.body;
+    // find the user by their email
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid email or password" });
+    }
+
+    // check if the password is correct
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid email or password" });
+    }
+
+    //generate token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    // return success response with token
+    res.json({
+      success: true,
+      token,
+      user: {
+        id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.log("Signin Error:", error.message);
+    res.status(500).json({ message: "Server error during login" });
+  }
+};
     
-        // find the user by their username
-        const user = await User.findOne({ email });
-    
-        if (!user) {
-          return res
-            .status(400)
-            .json({ success: false, message: "Invalid email Or Password" });
-        }
-    
-        // check if the password is correct
-        // const isMatch = await bcrypt.compare(password, user.password);
-        // if (!isMatch) {
-        //   return res
-        //     .status(400)
-        //     .json({ success: false, message: "Invalid email Or Password" });
-        // }
-    
-        //generate token
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-          expiresIn: "1d",
-        });
-    
-        // return success response with token
-        res.json({
-          success: true,
-          token,
-          user: {
-            id: user._id,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            username: user.username,
-            email: user.email,
-          },
-        });
-      } catch (error) {
-        console.log("Signin Error:", error);
-        res.status(500).json({ message: "Server error during login" });
-      }
-    };
-    
-    module.exports = { signup, signin };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = { signup, signin };
